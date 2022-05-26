2491440de06ff054b56b601cee888ae83149d2ca
admin <admin@Bcalab4-m14>
2022-05-26 09:22:21 +0530

    thermos program

---
+++ program1/src/program1/Thermos.java
@@ -0,0 +1,14 @@
+package program1;
+
+public class Thermos {
+
+	public Thermos() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello suraj");
+	}
+
+}
